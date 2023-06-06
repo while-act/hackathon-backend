a698d09867ae1a2ba77cd96d68baa479d1ae76ac
Teleфonчиk <matvey-sizov@mail.ru>
2023-06-06 18:35:03 +0300

    fix: update cookie params

--- pkg/conf/config.go
+++ pkg/conf/config.go
@@ -18,7 +18,7 @@ type Config struct {
 
 	Session struct {
 		CookieName string        `yaml:"cookie_name" env:"COOKIE_NAME" env-default:"session_id"`
-		CookiePath string        `yaml:"cookie_path" env:"COOKIE_PATH" env-default:"/api"`
+		CookiePath string        `yaml:"cookie_path" env:"COOKIE_PATH" env-default:"hackathon.frkam.dev/api"`
 		Duration   time.Duration `yaml:"duration" env:"COOKIE_DURATION" env-default:"720h"`
 	} `yaml:"session"`
 
